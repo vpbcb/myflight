e3380cc726422a28dc96eac56632443e6718db67
vpbcb <vpbcb1@gmail.com>
2026-06-11 19:42:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'myflight_v.260611-airp-navaids-1';
+const CACHE_NAME = 'myflight_v.260611-12';
 const ASSET_FETCH_TIMEOUT_MS = 20000;
 
 // New worker activates only after every critical asset is cached.
