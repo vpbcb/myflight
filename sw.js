eef23782e3c0f9464295aa99f92c1ddce6eb4b68
vpbcb <vpbcb1@gmail.com>
2026-06-13 09:33:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'myflight_v.260613-6';
+const CACHE_NAME = 'myflight_v.260613-7';
 const ASSET_FETCH_TIMEOUT_MS = 20000;
 
 // New worker activates only after every critical asset is cached.
