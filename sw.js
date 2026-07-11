2b546b217f940cf485ab8c305b4a90d78f51a63a
vpbcb <vpbcb1@gmail.com>
2026-07-11 15:44:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'myflight_v.260711-5';
+const CACHE_NAME = 'myflight_v.260711-6';
 const ASSET_FETCH_TIMEOUT_MS = 12000;
 const SLOW_ASSET_LOG_MS = 2500;
 const FIREBASE_RUNTIME_ASSETS = [
